5ba29f1b003845c3283f1e5c2dc1aa3089948060
bloodlaac <anaevaulia5@gmail.com>
2025-07-02 17:02:09 +0400

    feat: add metric to experiment card

--- src/sniffnet/api/routes/experiments.py
+++ src/sniffnet/api/routes/experiments.py
@@ -1,10 +1,10 @@
 from typing import Annotated
-from sniffnet.schemas.experiments import ExperimentRequest, ExperimentJoined
+from sniffnet.schemas.experiments import CreateExperimentRequest, ExperimentJoined
 from sniffnet.schemas.training_configs import ConfigRequest
 from sqlalchemy.orm import Session
-from sqlalchemy import select
-from sniffnet.database.db_models import Experiment, TrainingConfig
+from sniffnet.database.db_models import Experiment, TrainingConfig, Metric
 from sniffnet.api.deps import get_database
+from datetime import datetime, timezone
 
 from fastapi import APIRouter, HTTPException, Depends
 
@@ -22,75 +22,56 @@ def get_experiment(
     db: Annotated[Session, Depends(get_database)]
 ):
     experiment = (
-      db.query(Experiment)
+        db.query(Experiment)
         .join(TrainingConfig)
         .filter(Experiment.experiment_id == id)
         .first()
     )
     if experiment is None:
         raise HTTPException(status_code=404, detail="Эксперимент не найден")
-    
+
     cfg: TrainingConfig = experiment.config
-    return ExperimentJoined(
-      experiment_id=experiment.experiment_id,
-      dataset_id=experiment.dataset_id,
-      config_id=experiment.config_id,
-      user_id=experiment.user_id,
-      start_time=experiment.start_time,
-      end_time=experiment.end_time,
-      batch_size=cfg.batch_size,
-      epochs_num=cfg.epochs_num,
-      loss_function=cfg.loss_function,
-      learning_rate=cfg.learning_rate,
-      optimizer=cfg.optimizer,
-      layers_num=cfg.layers_num,
-      neurons_num=cfg.neurons_num,
-    )
 
-
-@router.post("/experiments", response_model=ExperimentJoined)
-def post_experiment(
-    payload: ExperimentRequest,
-    db: Annotated[Session, Depends(get_database)]
-) -> ExperimentJoined:
-    config_data = ConfigRequest(
-        epochs_num=payload.epochs_num,
-        batch_size=payload.batch_size,
-        loss_function=payload.loss_function,
-        learning_rate=payload.learning_rate,
-        optimizer=payload.optimizer,
-        layers_num=payload.layers_num,
-        neurons_num=payload.neurons_num,
+    metric = (
+        db.query(Metric)
+        .filter(Metric.config_id == cfg.config_id)
+        .first()
     )
-    db_config = TrainingConfig(**config_data.model_dump())
-    db.add(db_config)
-    db.commit()
-    db.refresh(db_config)
-
-    exp_data = payload.model_dump(exclude={
-        "epochs_num", "batch_size", "loss_function",
-        "learning_rate", "optimizer", "layers_num", "neurons_num",
-    })
-    db_experiment = Experiment(
-        **exp_data,
-        config_id=db_config.config_id
-    )
-    db.add(db_experiment)
-    db.commit()
-    db.refresh(db_experiment)
 
     return ExperimentJoined(
-        experiment_id=db_experiment.experiment_id,
-        dataset_id=db_experiment.dataset_id,
-        config_id=db_config.config_id,
-        user_id=db_experiment.user_id,
-        start_time=db_experiment.start_time,
-        end_time=db_experiment.end_time,
-        batch_size=db_config.batch_size,
-        epochs_num=db_config.epochs_num,
-        loss_function=db_config.loss_function,
-        learning_rate=db_config.learning_rate,
-        optimizer=db_config.optimizer,
-        layers_num=db_config.layers_num,
-        neurons_num=db_config.neurons_num,
+        experiment_id=experiment.experiment_id,
+        dataset_id=experiment.dataset_id,
+        config_id=experiment.config_id,
+        user_id=experiment.user_id,
+        start_time=experiment.start_time,
+        end_time=experiment.end_time,
+        batch_size=cfg.batch_size,
+        epochs_num=cfg.epochs_num,
+        loss_function=cfg.loss_function,
+        learning_rate=cfg.learning_rate,
+        optimizer=cfg.optimizer,
+        layers_num=cfg.layers_num,
+        neurons_num=cfg.neurons_num,
+        train_accuracy=metric.train_accuracy if metric else None,
+        train_loss=metric.train_loss if metric else None,
     )
+
+
+@router.post("/experiments")
+def create_experiment(request: CreateExperimentRequest, db: Session = Depends(get_database)):
+    config = TrainingConfig(**request.config.model_dump())
+    db.add(config)
+    db.commit()
+    db.refresh(config)
+
+    experiment = Experiment(
+        user_id=request.user_id,
+        dataset_id=request.dataset_id,
+        config_id=config.config_id,
+        start_time=datetime.now(timezone.utc)
+    )
+    db.add(experiment)
+    db.commit()
+    db.refresh(experiment)
+
+    return {"experiment_id": experiment.experiment_id}
--- src/sniffnet/schemas/experiments.py
+++ src/sniffnet/schemas/experiments.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import BaseModel, ConfigDict
 from datetime import datetime
 
@@ -8,12 +9,7 @@ class ExperimentBase(BaseModel):
 
     model_config = ConfigDict(from_attributes=True)
 
-class ExperimentRequest(BaseModel):
-    dataset_id: int
-    user_id: int
-    start_time: datetime
-    end_time: datetime
-
+class TrainingConfigCreate(BaseModel):
     epochs_num: int
     batch_size: int
     loss_function: str
@@ -22,18 +18,19 @@ class ExperimentRequest(BaseModel):
     layers_num: int
     neurons_num: int
 
-    model_config = ConfigDict(from_attributes=True)
+class CreateExperimentRequest(BaseModel):
+    user_id: int
+    dataset_id: int
+    config: TrainingConfigCreate
 
 class ExperimentJoined(BaseModel):
     experiment_id: int
     dataset_id: int
     config_id: int
     user_id: int
-
     start_time: datetime
-    end_time: datetime
+    end_time: Optional[datetime]
 
-    # поля из TrainingConfig
     batch_size: int
     epochs_num: int
     loss_function: str
@@ -41,5 +38,8 @@ class ExperimentJoined(BaseModel):
     optimizer: str
     layers_num: int
     neurons_num: int
+
+    train_accuracy: Optional[float] = None
+    train_loss: Optional[float] = None
 
     model_config = ConfigDict(from_attributes=True)
