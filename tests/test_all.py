29dde396b12bfb70715538521a86607eab260bf6
bloodlaac <anaevaulia5@gmail.com>
2025-09-02 21:37:07 +0400

    core: some fixs

---
+++ tests/test_all.py
@@ -0,0 +1,69 @@
+import pytest
+
+
+def test_create_user():
+    assert True
+
+
+def test_login():
+    assert True
+
+
+def test_create_dataset():
+    assert True
+
+
+def test_get_datasets():
+    assert True
+
+
+def test_delete_dataset():
+    assert True
+
+
+def test_create_experiment():
+    assert True
+
+
+def test_get_experiment():
+    assert True
+
+
+def test_delete_experiment():
+    assert True
+
+
+def test_create_metric():
+    assert True
+
+
+def test_get_metric():
+    assert True
+
+
+def test_delete_metric():
+    assert True
+
+
+def test_create_model():
+    assert True
+
+
+def test_get_model():
+    assert True
+
+
+def test_delete_model():
+    assert True
+
+
+def test_create_config():
+    assert True
+
+
+def test_get_config():
+    assert True
+
+
+def test_delete_config():
+    assert True
